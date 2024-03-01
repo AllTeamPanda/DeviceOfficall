b605f6cf3c83b9c7ab4d64b6c85533bea747d4ef
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2024-03-01 23:50:38 +0700

    Update string.py

--- device/string.py
+++ device/string.py
@@ -48,10 +48,37 @@ def session():
     ip = ipaddress.ip_address(IP_ADDRES).packed
     trings = CURRENT_VERSION + StringSession.encode(struct.pack(_STRUCT_PREFORMAT.format(len(ip)), 2, ip, 443, PUBLIC_KEY,))
     print(trings)
-    
+
+
+_PYRO_FORM = {351: ">B?256sI?", 356: ">B?256sQ?", 362: ">BI?256sQ?"}
+
+
+def encode():
+     ppk = input("Please enter your STRING: ")     
+     if len(ppk) in _PYRO_FORM.keys():
+         data_ = struct.unpack(
+                 _PYRO_FORM[len(ppk)],
+                 base64.urlsafe_b64decode(ppk + "=" * (-len(ppk) % 4)),
+         )
+         if len(ppk) in [351, 356]:
+             auth_id = 2
+         else:
+             auth_id = 3
+
+         dc_id, api_id, tesmode, auth_key = data_[0], data_[1], data_[2], data_[auth_id]       
+     try:
+         et = dc_id, auth_key
+         atc = et
+         tes = tesmode
+         print(
+            f"**=>> Decoded Text :** `{data_}`\n\n**=>> OUTPUT :**\n`{atc}` \n\n Mode> {tesmode}\n\n  and Api Id: {api_id} "
+         )
+     except Exception as p:
+         print(event, "**ERROR :** " + str(p))
+
 def main():
     print("Waiting...")
-    session()
+    encode()
 
 main()
     
