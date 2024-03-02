18642e58775b06b18cb0c5fea864a8cacfb1a89c
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2024-03-02 09:38:27 +0700

    Update string.py

--- device/string.py
+++ device/string.py
@@ -72,7 +72,7 @@ def encode():
              auth_id = 3
 
          dc_id, auth_key = data_[0], data_[auth_id]   
-         stringtele = CURRENT_VERSION + StringSession.encode(struct.pack(_STRUCT_PREFORMAT.format(len(4)), dc_id, ipaddress.ip_address(DC_IPV4[dc_id]).packed, 443, auth_key,))
+         stringtele = StringSession.encode(struct.pack(CURRENT_VERSION + _STRUCT_PREFORMAT.format(len(4)), dc_id, ipaddress.ip_address(DC_IPV4[dc_id]).packed, 443, auth_key,))
          print(f"{stringtele}")
 
 def encodes():
