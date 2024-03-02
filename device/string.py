1b5a19a3000a116ad62f30a6d4fd9b8de31f4657
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2024-03-02 15:05:59 +0700

    Update string.py

--- device/string.py
+++ device/string.py
@@ -119,11 +119,7 @@ def encodes():
          data_ = struct.unpack(
                 _STRUCT_PREFORMAT.format(ip_len), StringSession.decode(ppk)
             )
-         if len(ppk) in [351, 356]:
-             auth_id = 2
-         else:
-             auth_id = 3
-                 
+         auth_id = 3        
          dc_id, auth_key = data_[0], data_[auth_id]       
          api_id = 6723770092
          test_mode = False
