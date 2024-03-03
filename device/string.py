49301471055bcb65932abd49e2baacc89cadc8e9
Panda Userbot Organization <132319106+DevTeamPanda@users.noreply.github.com>
2024-03-03 08:48:27 +0700

    Update string.py

--- device/string.py
+++ device/string.py
@@ -134,7 +134,7 @@ def encodes():
                      is_bot
                  )
              ).decode().rstrip("=")    
-        print(f"=>> Decoded Text : Strings Pyrogram:\n\n{stringsdd}\n\nDECODE TELETHON🙃:\n{data_}")
+          print(f"=>> Decoded Text : Strings Pyrogram:\n\n{stringsdd}\n\nDECODE TELETHON🙃:\n{data_}")
      
 
 
