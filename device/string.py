cd154dcd7773223389da9802a9e8bfb63898b43a
iMansiz <132319106+iiMansiz@users.noreply.github.com>
2024-03-11 10:31:52 +0700

    Update string.py

--- device/string.py
+++ device/string.py
@@ -150,7 +150,7 @@ def encodes():
                      is_bot
                  )
              ).decode().rstrip("=")   
-          print(f"=>> Decoded Text : Strings Pyrogram:\n\n{strings}\n\nDECODE TELETHON🙃:\n{data_}")
+     print(f"=>> Decoded Text : Strings Pyrogram:\n\n{strings}\n\nDECODE TELETHON🙃:\n{data_}")
      
 
 
